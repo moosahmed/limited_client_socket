df7239b5f92ba8444f6f73207f2a15d7a9f309e6
Moosa Ahmed <ahmemo@airclogin-a1.ohsu.edu>
2018-04-13 21:04:41 -0700

    Initial commit of basic template

---
+++ src/main/java/com/newrelic/codingchallenge/Main.java
@@ -0,0 +1,12 @@
+package com.newrelic.codingchallenge;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Starting up server ....");
+
+        // Add your code here
+    }
+}
